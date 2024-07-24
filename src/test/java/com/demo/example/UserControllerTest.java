28c75368fcd96c13be99e1597540b65e4dca01a3
phu2kor <prasad.rahul@in.bosch.com>
2024-07-24 11:12:35 +0530

    Demo Code:  unit test with tight couple code

---
+++ src/test/java/com/demo/example/UserControllerTest.java
@@ -0,0 +1,16 @@
+package com.demo.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+}
